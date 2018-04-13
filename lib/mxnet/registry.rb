96020b697d868e603315cea45452dbabae9b5a72
Kenta Murata <mrkn@mrkn.jp>
2018-04-13 10:08:49 +0900

    Add MXNet::Registry

---
+++ lib/mxnet/registry.rb
@@ -0,0 +1,93 @@
+require 'json'
+
+module MXNet
+  module Registry
+    @registries = {}
+
+    class Manager
+      def initialize(base_class, nickname)
+        @registry = registry_for(base_class)
+        @base_class = base_class
+        @nickname = nickname
+      end
+
+      private def registry_for(klass)
+        registries = MXNet::Registry.instance_variable_get(:@registries)
+        registries[klass] ||= {}
+      end
+
+      # Register functions
+      def register(klass, name=nil)
+        unless klass < @base_class
+          raise TypeError, "Can only register subclass of #{@base_class}"
+        end
+        name = klass.name[/:?(\w+)\z/, 1].downcase if name.nil?
+        name = name.to_sym
+        if @registry.has_key?(name)
+          warn "\033[91mNew #{@nickname} #{klass} registered with name #{name}" +
+               " is overriding existing #{@nickname} #{@registry[name]}\033[0m"
+        end
+        @registry[name] = klass
+      end
+
+      def alias(klass, *aliases)
+        aliases.each do |name|
+          register(klass, name)
+        end
+        klass
+      end
+
+      def create(*args, **kwargs)
+        if args.length > 0
+          name = args.shift
+        else
+          name = kwargs.delete(@nickname)
+        end
+
+        case name
+        when @base_class
+          unless args.length == 0  && kwargs.length == 0
+            raise ArgumentError,
+                  "#{@nickname} is already an instance. " +
+                  "Additional arguments are invalid"
+          end
+          return name
+        when Hash
+          return create(**name)
+        when String, ::Symbol
+          name = name.to_s
+        else
+          if name.respond_to?(:to_str)
+            name = name.to_str
+          else
+            raise ArgumentError, "#{@nickname} must be a String or a Symbol"
+          end
+        end
+
+        # The variable `name`` is ensured to be a String, here.
+
+        if name.start_with?('[') || name.start_with?('{')
+          unless args.length == 0 && kwargs.length == 0
+            raise ArgumentError, "Additional arguments for JSON is invalid."
+          end
+          kwargs = JSON.load(name)
+          if kwargs.is_a?(Hash)
+            return create(**kwargs.transform_keys(&:to_sym))
+          else
+            name, kwargs = *kwargs
+            kwargs = (kwargs || {}).transform_keys(&:to_sym)
+            return create(name, **kwargs)
+          end
+        end
+
+        name = name.downcase.to_sym
+        unless @registry.has_key?(name)
+          raise ArgumentError,
+                "#{name} is not registered. " +
+                "Please register it for #{@nickname} first"
+        end
+        return @registry[name].new(*args, **kwargs)
+      end
+    end
+  end
+end
---
+++ spec/mxnet/registry_spec.rb
@@ -0,0 +1,129 @@
+require 'spec_helper'
+
+RSpec.describe MXNet::Registry::Manager do
+  let(:namespace) { Module.new }
+
+  before do
+    class namespace::TestClass
+      def initialize(*args, **kwargs)
+        @args = args
+        @params = kwargs
+      end
+
+      attr_reader :args, :params
+    end
+
+    class namespace::TestSubclass1 < namespace::TestClass
+    end
+
+    class namespace::TestSubClass1 < namespace::TestClass
+    end
+
+    class namespace::TestSubclass2 < namespace::TestClass
+    end
+
+    class namespace::TestOtherClass
+    end
+  end
+
+  subject(:regman) do
+    MXNet::Registry::Manager.new(namespace::TestClass, :test_class)
+  end
+
+  let(:internal_registry) do
+    regman.instance_variable_get(:@registry)
+  end
+
+  describe '#register' do
+    specify do
+      expect(regman.register(namespace::TestSubclass1)).to equal(namespace::TestSubclass1)
+      expect(regman.register(namespace::TestSubclass2)).to equal(namespace::TestSubclass2)
+    end
+
+    specify do
+      regman.register(namespace::TestSubclass1)
+      expect(internal_registry.length).to eq(1)
+      expect(internal_registry.keys).to eq([:testsubclass1])
+
+      regman.register(namespace::TestSubclass1)
+      expect(internal_registry.length).to eq(1)
+      expect(internal_registry.keys).to eq([:testsubclass1])
+
+      regman.register(namespace::TestSubClass1)
+      expect(internal_registry.length).to eq(1)
+      expect(internal_registry.keys).to eq([:testsubclass1])
+      expect(internal_registry[:testsubclass1]).to equal(namespace::TestSubClass1)
+
+      regman.register(namespace::TestSubClass1, :foo)
+      expect(internal_registry.length).to eq(2)
+      expect(internal_registry.keys).to eq([:testsubclass1, :foo])
+      expect(internal_registry[:testsubclass1]).to equal(namespace::TestSubClass1)
+      expect(internal_registry[:foo]).to equal(namespace::TestSubClass1)
+
+      regman.register(namespace::TestSubclass2, :foo)
+      expect(internal_registry.length).to eq(2)
+      expect(internal_registry.keys).to eq([:testsubclass1, :foo])
+      expect(internal_registry[:testsubclass1]).to equal(namespace::TestSubClass1)
+      expect(internal_registry[:foo]).to equal(namespace::TestSubclass2)
+
+      expect { regman.register(namespace::TestOtherClass) }.to raise_error(TypeError)
+      expect(internal_registry.length).to eq(2)
+      expect(internal_registry.keys).to eq([:testsubclass1, :foo])
+      expect(internal_registry[:testsubclass1]).to equal(namespace::TestSubClass1)
+      expect(internal_registry[:foo]).to equal(namespace::TestSubclass2)
+    end
+  end
+
+  describe '#alias' do
+    specify do
+      expect(regman.alias(namespace::TestSubclass1, :bar, :baz)).to equal(namespace::TestSubclass1)
+      expect(internal_registry.length).to eq(2)
+      expect(internal_registry.keys).to eq([:bar, :baz])
+    end
+  end
+
+  describe '#create' do
+    before do
+      regman.register(namespace::TestSubclass1)
+      regman.register(namespace::TestSubclass2, :foo)
+    end
+
+    specify 'name only' do
+      expect(regman.create(:testsubclass1)).to be_a(namespace::TestSubclass1)
+      expect(regman.create(:testsubclass1, a: 1, b: 2).params).to eq({a: 1, b: 2})
+      expect(regman.create(:TESTSUBCLASS1)).to be_a(namespace::TestSubclass1)
+
+      expect(regman.create(:foo)).to be_a(namespace::TestSubclass2)
+      expect(regman.create(:foo, 1, 2).args).to eq([1, 2])
+      expect(regman.create(:foo, c: 2, d: 3).params).to eq({c: 2, d: 3})
+      expect(regman.create(:FOO)).to be_a(namespace::TestSubclass2)
+
+      expect(regman.create(test_class: :TestSubclass1)).to be_a(namespace::TestSubclass1)
+      expect(regman.create(test_class: :foo)).to be_a(namespace::TestSubclass2)
+      expect(regman.create(test_class: :FOO)).to be_a(namespace::TestSubclass2)
+
+      expect { regman.create(:unknown_name) }.to raise_error(ArgumentError)
+      expect { regman.create(1) }.to raise_error(ArgumentError)
+    end
+
+    specify 'instance' do
+      x = namespace::TestSubclass1.new
+      expect(regman.create(x)).to equal(x)
+      expect { regman.create(x, 1) }.to raise_error(ArgumentError)
+      expect { regman.create(x, a: 1) }.to raise_error(ArgumentError)
+    end
+
+
+    specify 'hash' do
+      expect(regman.create({test_class: :foo, a: 1})).to be_a(namespace::TestSubclass2)
+      expect(regman.create({test_class: :FOO, a: 1})).to be_a(namespace::TestSubclass2)
+    end
+
+    specify 'json' do
+      expect(regman.create([:foo].to_json)).to be_a(namespace::TestSubclass2)
+      expect(regman.create([:foo, {a: 1, b: 2}].to_json).params).to eq({a: 1, b: 2})
+      expect(regman.create({test_class: :foo}.to_json)).to be_a(namespace::TestSubclass2)
+      expect(regman.create({test_class: :foo, a: 1, b: 2}.to_json).params).to eq({a: 1, b: 2})
+    end
+  end
+end
