4d41e82f1c6351899c2b6adfbe75c543071a148a
Kenta Murata <mrkn@mrkn.jp>
2018-04-17 01:18:35 +0900

    Add missing files for MXNet::Gluon::Data

---
+++ lib/mxnet/gluon.rb
@@ -0,0 +1,8 @@
+require 'mxnet'
+
+module MXNet
+  module Gluon
+  end
+end
+
+require 'mxnet/gluon/data'
---
+++ lib/mxnet/gluon/data.rb
@@ -0,0 +1,9 @@
+module MXNet
+  module Gluon
+    module Data
+    end
+  end
+end
+
+require_relative 'data/dataset'
+require_relative 'data/vision/mnist'
