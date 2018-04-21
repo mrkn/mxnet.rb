91fb6bc9b26b3315fbd76b1b94b6355703e4a1e1
Kenta Murata <mrkn@mrkn.jp>
2018-04-21 16:42:22 +0900

    Add Autograd.backward

--- ext/mxnet/autograd.c
+++ ext/mxnet/autograd.c
@@ -1,5 +1,96 @@
 #include "mxnet_internal.h"
 
+/* Compute the gradients of heads w.r.t previously marked variables.
+ */
+static VALUE
+autograd_s_backward(int argc, VALUE *argv, VALUE mod)
+{
+  VALUE heads, opts, head_grads = Qnil;
+  int retain_graphs = 0, train_mode = 1;
+
+  VALUE head_handles_str, head_grad_handles_str;
+  NDArrayHandle *head_handles, *head_grad_handles = NULL;
+  mx_uint i, heads_len;
+
+  rb_scan_args(argc, argv, "1:", &heads, &opts);
+  if (!NIL_P(opts)) {
+    static ID keywords[3];
+    VALUE vals[3];
+
+    if (keywords[0] == 0) {
+      keywords[0] = rb_intern("head_grads");
+      keywords[1] = rb_intern("retain_graph");
+      keywords[2] = rb_intern("train_mode");
+    }
+
+    rb_get_kwargs(opts, keywords, 0, 3, vals);
+
+    if (vals[0] != Qundef) {
+      head_grads = vals[0];
+    }
+    if (vals[1] != Qundef) {
+      retain_graphs = RTEST(vals[1]);
+    }
+    if (vals[2] != Qundef) {
+      train_mode = RTEST(vals[2]);
+    }
+  }
+
+  if (mxnet_is_ndarray(heads)) {
+    VALUE ary = rb_ary_new_capa(1);
+    rb_ary_push(ary, heads);
+    heads = ary;
+  }
+
+  heads = rb_convert_type(heads, T_ARRAY, "Array", "to_ary");
+#if SIZEOF_LONG > SIZEOF_INT
+  if (RARRAY_LEN(heads) > UINT_MAX) {
+    rb_raise(rb_eArgError, "too many heads");
+  }
+#endif
+
+  heads_len = (mx_uint)RARRAY_LEN(heads);
+  head_handles_str = rb_str_tmp_new(sizeof(NDArrayHandle) * heads_len);
+  head_handles = (NDArrayHandle *)RSTRING_PTR(head_handles_str);
+  for (i = 0; i < heads_len; ++i) {
+    head_handles[i] = mxnet_ndarray_get_handle(RARRAY_AREF(heads, i));
+  }
+
+  if (!NIL_P(head_grads)) {
+    if (mxnet_is_ndarray(head_grads)) {
+      VALUE ary = rb_ary_new_capa(1);
+      rb_ary_push(ary, head_grads);
+      head_grads = ary;
+    }
+
+    head_grads = rb_convert_type(head_grads, T_ARRAY, "Array", "to_ary");
+    if (RARRAY_LEN(heads) != RARRAY_LEN(head_grads)) {
+      rb_raise(rb_eArgError, "haeds and head_grads must be arrays of the same length");
+    }
+
+    head_grad_handles_str = rb_str_tmp_new(sizeof(NDArrayHandle) * heads_len);
+    head_grad_handles = (NDArrayHandle *)RSTRING_PTR(head_grad_handles_str);
+    for (i = 0; i < heads_len; ++i) {
+      head_grad_handles[i] = mxnet_ndarray_get_handle(RARRAY_AREF(head_grads, i));
+    }
+  }
+
+  CHECK_CALL(
+    MXNET_API(MXAutogradBackwardEx)(
+      heads_len,
+      head_handles,
+      head_grad_handles,
+      0,
+      NULL,
+      retain_graphs,
+      0,
+      train_mode,
+      NULL,
+      NULL));
+
+  return Qnil;
+}
+
 /* Set status to recording/not recording.
  * When recording, graph will be constructed for gradient computation.
  *
@@ -51,6 +142,7 @@ mxnet_init_autograd(void)
   VALUE mAutograd;
 
   mAutograd = rb_const_get_at(mxnet_mMXNet, rb_intern("Autograd"));
+  rb_define_singleton_method(mAutograd, "backward", autograd_s_backward, -1);
   rb_define_singleton_method(mAutograd, "set_recording", autograd_s_set_recording, 1);
   rb_define_singleton_method(mAutograd, "set_training", autograd_s_set_training, 1);
   rb_define_singleton_method(mAutograd, "recording?", autograd_s_recording_p, 0);
--- spec/mxnet/autograd_spec.rb
+++ spec/mxnet/autograd_spec.rb
@@ -6,4 +6,37 @@ RSpec.describe MXNet::Autograd do
       expect { |b| MXNet::Autograd.record(&b) }.to yield_control
     end
   end
+
+  describe '.backward' do
+    let(:x) { MXNet::NDArray.array([1, 2, 3, 4, 5]) }
+    let(:y) { MXNet::NDArray.array([10]) }
+
+    let(:w) { MXNet::NDArray::Random.normal(shape: x.shape) }
+    let(:b) { MXNet::NDArray::Random.uniform(shape: [1]) }
+
+    let(:loss) do
+      w.attach_grad
+      b.attach_grad
+
+      expect((w.grad - MXNet::NDArray.zeros_like(w)).abs.max.as_scalar).to eq(0.0)
+      expect((b.grad - MXNet::NDArray.zeros_like(b)).abs.max.as_scalar).to eq(0.0)
+
+      MXNet::Autograd.record do
+        z = MXNet::NDArray.dot(w, x) + b
+        (y - z)**2 / 2.0
+      end
+    end
+
+    specify do
+      MXNet::Autograd.backward(loss)
+      expect((w.grad - MXNet::NDArray.zeros_like(w)).abs.max.as_scalar).not_to eq(0.0)
+      expect((b.grad - MXNet::NDArray.zeros_like(b)).abs.max.as_scalar).not_to eq(0.0)
+    end
+
+    specify do
+      MXNet::Autograd.backward([loss])
+      expect((w.grad - MXNet::NDArray.zeros_like(w)).abs.max.as_scalar).not_to eq(0.0)
+      expect((b.grad - MXNet::NDArray.zeros_like(b)).abs.max.as_scalar).not_to eq(0.0)
+    end
+  end
 end
