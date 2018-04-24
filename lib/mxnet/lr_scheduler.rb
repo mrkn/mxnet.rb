f54cc5c7c45c6870164bc6f5c3f0d60d9363f9fb
Kenta Murata <mrkn@mrkn.jp>
2018-04-24 15:18:01 +0900

    Add MXNet::LRScheduler::MultiFactorScheduler

--- lib/mxnet/lr_scheduler.rb
+++ lib/mxnet/lr_scheduler.rb
@@ -55,7 +55,42 @@ module MXNet
       alias_method :update, :call
     end
 
-    # TODO: class MultiFactorScheduler < Base
+    # Reduce the learning rate by given a list of steps.
+    class MultiFactorScheduler < Base
+      def initialize(step:, factor: 1)
+        super()
+        unless step.is_a?(Array) && step.length > 1
+          raise ArgumentError, "Schedule step must be an Array with more than 1 items"
+        end
+        step.each_cons(2) do |s0, s1|
+          raise ArgumentError, "Schedule step must be an increasing integer array" if s1 <= s0
+          raise ArgumentError, "Schedule step must be greater than or equal to 1" if s0 < 1 || s1 < 1
+        end
+        raise ArgumentError, "Factor must be no more than 1 to make lr reduce" if factor > 1
+        @step = step
+        @cur_step_ind = 0
+        @factor = factor
+        @count = 0
+      end
+
+      attr_reader :step, :cur_step_ind, :factor, :count
+
+      def call(num_update)
+        while @cur_step_ind < @step.length
+          if num_update > @step[@cur_step_ind]
+            @count = @step[@cur_step_ind]
+            @cur_step_ind += 1
+            @base_lr *= @factor
+            puts("Update[%d]: Change learning rate to %0.5e" % [num_update, @base_lr])
+          else
+            return @base_lr
+          end
+        end
+        return @base_lr
+      end
+
+      alias_method :update, :call
+    end
 
     # TODO: class PolyScheduler < Base
   end
--- spec/mxnet/lr_scheduler_spec.rb
+++ spec/mxnet/lr_scheduler_spec.rb
@@ -35,3 +35,72 @@ RSpec.describe MXNet::LRScheduler::FactorScheduler do
     expect(lrsched.count).to eq(20)
   end
 end
+
+RSpec.describe MXNet::LRScheduler::MultiFactorScheduler do
+  specify do
+    expect {
+      MXNet::LRScheduler::MultiFactorScheduler.new(step: 10, factor: 0.2)
+    }.to raise_error(ArgumentError, /step must be an Array with more than 1 items/)
+    expect {
+      MXNet::LRScheduler::MultiFactorScheduler.new(step: [10], factor: 0.2)
+    }.to raise_error(ArgumentError, /step must be an Array with more than 1 items/)
+  end
+
+  specify do
+    expect {
+      MXNet::LRScheduler::MultiFactorScheduler.new(step: [10, 5], factor: 0.2)
+    }.to raise_error(ArgumentError, /step must be an increasing integer array/)
+  end
+
+  specify do
+    expect {
+      MXNet::LRScheduler::MultiFactorScheduler.new(step: [0, 1, 2], factor: 0.2)
+    }.to raise_error(ArgumentError, /step must be greater than or equal to 1/)
+  end
+
+  specify do
+    lrsched = MXNet::LRScheduler::MultiFactorScheduler.new(step: [30, 60, 90], factor: 0.2)
+    lrsched.base_lr = 0.1
+
+    expect(lrsched.step).to eq([30, 60, 90])
+    expect(lrsched.factor).to eq(0.2)
+    expect(lrsched.count).to eq(0)
+    expect(lrsched.base_lr).to eq(0.1)
+
+    expect(lrsched.update(1)).to eq(0.1)
+    expect(lrsched.base_lr).to eq(0.1)
+    expect(lrsched.count).to eq(0)
+
+    expect(lrsched.update(30)).to eq(0.1)
+    expect(lrsched.base_lr).to eq(0.1)
+    expect(lrsched.count).to eq(0)
+
+    expect(lrsched.update(31)).to be_within(1e-12).of(0.02)
+    expect(lrsched.base_lr).to be_within(1e-12).of(0.02)
+    expect(lrsched.count).to eq(30)
+
+    expect(lrsched.update(32)).to be_within(1e-12).of(0.02)
+    expect(lrsched.base_lr).to be_within(1e-12).of(0.02)
+    expect(lrsched.count).to eq(30)
+
+    expect(lrsched.update(60)).to be_within(1e-12).of(0.02)
+    expect(lrsched.base_lr).to be_within(1e-12).of(0.02)
+    expect(lrsched.count).to eq(30)
+
+    expect(lrsched.update(61)).to be_within(1e-12).of(0.004)
+    expect(lrsched.base_lr).to be_within(1e-12).of(0.004)
+    expect(lrsched.count).to eq(60)
+
+    expect(lrsched.update(62)).to be_within(1e-12).of(0.004)
+    expect(lrsched.base_lr).to be_within(1e-12).of(0.004)
+    expect(lrsched.count).to eq(60)
+
+    expect(lrsched.update(90)).to be_within(1e-12).of(0.004)
+    expect(lrsched.base_lr).to be_within(1e-12).of(0.004)
+    expect(lrsched.count).to eq(60)
+
+    expect(lrsched.update(91)).to be_within(1e-12).of(0.0008)
+    expect(lrsched.base_lr).to be_within(1e-12).of(0.0008)
+    expect(lrsched.count).to eq(90)
+  end
+end
