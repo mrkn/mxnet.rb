0f23627db9e5358a152f524b3c5620fa58c6067b
Kenta Murata <mrkn@mrkn.jp>
2018-06-27 12:04:56 +0900

    Add tests for symbolize dtype value

---
+++ spec/mxnet/dtype_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+RSpec.describe MXNet::DType do
+  describe '.id2name' do
+    def id2name(id)
+      MXNet::DType.id2name(id)
+    end
+
+    specify do
+      expect(id2name(0)).to eq(:float32)
+    end
+  end
+
+  describe '.name2id' do
+    def name2id(name)
+      MXNet::DType.name2id(name)
+    end
+
+    specify do
+      expect(name2id(:float32)).to eq(0)
+    end
+
+    specify do
+      expect(name2id('float32')).to eq(0)
+    end
+  end
+end
+
+RSpec.describe 'dtype' do
+  specify 'dtype is always symbol' do
+    x = MXNet::NDArray.zeros([3])
+    expect(x.dtype).to eq(:float32)
+  end
+
+  specify 'dtype can be specified as a string' do
+    x = MXNet::NDArray.zeros([3], dtype: 'float32')
+    expect(x.dtype).to eq(:float32)
+
+    x = MXNet::NDArray.zeros([3], dtype: 'float64')
+    expect(x.dtype).to eq(:float64)
+  end
+
+  specify 'dtype can be specified as an integer' do
+    x = MXNet::NDArray.zeros([3], dtype: 'float32')
+    expect(x.dtype).to eq(:float32)
+
+    x = MXNet::NDArray.zeros([3], dtype: 'float64')
+    expect(x.dtype).to eq(:float64)
+  end
+end
