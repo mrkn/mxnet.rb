86137f016dda966d10ad500f91e7f9ddcdd94b24
Todd Sundsted <todd@sundsted.com>
2018-08-11 10:32:16 -0400

    No-op: use nested module/class definitions.

--- lib/mxnet/gluon/nn/activations.rb
+++ lib/mxnet/gluon/nn/activations.rb
@@ -1,32 +1,36 @@
 require 'mxnet/gluon/block'
 require 'mxnet/gluon/nn'
 
-module MXNet::Gluon::NN
-  ##
-  # Applies an activation function.
-  #
-  class Activation < MXNet::Gluon::HybridBlock
-    ##
-    # Creates a new instance.
-    #
-    # ====Parameters
-    #
-    # +activation+:: (string)
-    #                Name of activation function to use.
-    #
-    def initialize(activation, **kwargs)
-      super(**kwargs)
-      @activation = activation
-    end
+module MXNet
+  module Gluon
+    module NN
+      ##
+      # Applies an activation function.
+      #
+      class Activation < MXNet::Gluon::HybridBlock
+        ##
+        # Creates a new instance.
+        #
+        # ====Parameters
+        #
+        # +activation+:: (string)
+        #                Name of activation function to use.
+        #
+        def initialize(activation, **kwargs)
+          super(**kwargs)
+          @activation = activation
+        end
 
-    def hybrid_forward(clazz, data)
-      clazz.Activation(data, act_type: @activation)
-    end
+        def hybrid_forward(clazz, data)
+          clazz.Activation(data, act_type: @activation)
+        end
 
-    private
+        private
 
-    def hint
-      @activation
+        def hint
+          @activation
+        end
+      end
     end
   end
 end
