b97bd46c263f7e4998faa5cf7d199af5ef35115d
Kenta Murata <mrkn@mrkn.jp>
2018-09-03 16:39:18 +0900

    docker for CI: support Ruby 2.3.7

--- docker/install_ruby.sh
+++ docker/install_ruby.sh
@@ -8,16 +8,16 @@ RUBYGEMS_VERSION=${RUBYGEMS_VERSION-2.7.7}
 BUNDLER_VERSION=${BUNDLER_VERSION-1.16.4}
 
 case $RUBY_VERSION in
-  "2.6.0-preview2")
+  2.6.0-preview2)
     RUBY_DOWNLOAD_SHA256=00ddfb5e33dee24469dd0b203597f7ecee66522ebb496f620f5815372ea2d3ec
     ;;
-  "2.5.1")
+  2.5.1)
     RUBY_DOWNLOAD_SHA256=886ac5eed41e3b5fc699be837b0087a6a5a3d10f464087560d2d21b3e71b754d
     ;;
-  "2.4.4")
+  2.4.4)
     RUBY_DOWNLOAD_SHA256=1d0034071d675193ca769f64c91827e5f54cb3a7962316a41d5217c7bc6949f0
     ;;
-  "2.3.7")
+  2.3.7)
     RUBY_DOWNLOAD_SHA256=c61f8f2b9d3ffff5567e186421fa191f0d5e7c2b189b426bb84498825d548edb
     ;;
   *)
@@ -30,6 +30,12 @@ buildDeps=$(cat /tmp/ruby_build_dep.txt)
 
 apt-get update
 apt-get install -y --no-install-recommends $buildDeps
+# Need to down grade openssl to 1.0.x for Ruby 2.3.x
+case $RUBY_VERSION in
+  2.3.*)
+    apt-get install -y --no-install-recommends libssl1.0-dev
+    ;;
+esac
 rm -rf /var/lib/apt/lists/*
 
 wget -O ruby.tar.xz "https://cache.ruby-lang.org/pub/ruby/${RUBY_MAJOR}/ruby-${RUBY_VERSION}.tar.xz"
