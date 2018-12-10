04b8464e9a036e3dfa4fddc6948f04f554a87e97
Kenta Murata <mrkn@mrkn.jp>
2018-12-10 14:50:11 +0900

    Add an example for the case of integer dtype
    This example is based on one written by @toddsundsted

--- spec/mxnet/symbol_spec.rb
+++ spec/mxnet/symbol_spec.rb
@@ -20,6 +20,14 @@ module MXNet
       end
 
       specify do
+        x = MXNet::Symbol.var(:x, shape: [2, 3], dtype: 1)
+        expect(x).to be_a(MXNet::Symbol)
+        expect(x.name).to eq(:x)
+        expect(x.attr(:__shape__)).to eq([2, 3].to_s)
+        expect(x.attr(:__dtype__)).to eq(:float64)
+      end
+
+      specify do
         expect {
           MXNet::Symbol.var(:x, shape: [2, 3], dtype: :invalid_dtype)
         }.to raise_error(ArgumentError, /:invalid_dtype/)
