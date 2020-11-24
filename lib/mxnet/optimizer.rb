b3ac32a7105c03d6078bcdd8282bb2a9a3f50536
Tjad Clark <tjataz@gmail.com>
2020-11-24 10:40:14 +0200

    #49: FTML optimiser implementation

--- lib/mxnet/optimizer.rb
+++ lib/mxnet/optimizer.rb
@@ -431,7 +431,68 @@ module MXNet
 
     registry_manager.register Signum
 
-    # TODO: FTML
+    class FTML < Base
+      # The FTML optimizer.
+  
+      # This class implements the optimizer described in
+      # *FTML - Follow the Moving Leader in Deep Learning*,
+      # available at http://proceedings.mlr.press/v70/zheng17a/zheng17a.pdf.
+  
+      # Denote time step by t. The optimizer updates the weight by::
+  
+      #     rescaled_grad = clip(grad * rescale_grad + wd * weight, clip_gradient)
+      #     v = beta2 * v + (1 - beta2) * square(rescaled_grad)
+      #     d_t = (1 - power(beta1, t)) / lr * square_root(v / (1 - power(beta2, t))) + epsilon)
+      #     z = beta1 * z + (1 - beta1) * rescaled_grad - (d_t - beta1 * d_(t-1)) * weight
+      #     weight = - z / d_t
+  
+      # For details of the update algorithm, see :class:`~mxnet.ndarray.ftml_update`.
+  
+      # This optimizer accepts the following parameters in addition to those accepted
+      # by :class:`.Optimizer`.
+  
+      # Parameters
+      # ----------
+      # +beta1+:: float, optional
+      #     0 < beta1 < 1. Generally close to 0.5.
+      # +beta2+:: float, optional
+      #     0 < beta2 < 1. Generally close to 1.
+      # +epsilon+:: float, optional
+      #     Small value to avoid division by 0.
+      
+      def initialize beta1: 0.6, beta2: 0.999, epsilon: 1e-8, **kwargs
+        super **kwargs
+        @beta1 = beta1
+        @beta2 = beta2
+        @epsilon = epsilon
+      end
+  
+      def create_state index, weight
+          [zeros(weight.shape, weight.context, dtype: weight.dtype), # d_0
+           zeros(weight.shape, weight.context, dtype: weight.dtype), # v_0
+           zeros(weight.shape, weight.context, dtype: weight.dtype)] # z_0
+      end
+  
+      def update index, weight, grad, state
+        raise unless weight.is_a? MXNet::NDArray
+        raise unless grad.is_a? MXNet::NDArray
+        update_count(index)
+        lr = get_lr(index)
+        wd = get_wd(index)
+        t = @index_update_count[index]
+
+        kwargs = {beta1: @beta1, beta2: @beta2, epsilon: @epsilon,
+                  rescale_grad: @rescale_grad, t: t}
+        
+        kwargs[:clip_grad] = @clip_gradient if @clip_gradient
+        
+        prev_d, prev_v, prev_z = state
+        MXNet::NDArray.ftml_update(weight, grad, prev_d, prev_v, prev_z, out=weight,
+                    lr=lr, wd=wd, **kwargs)
+      end
+    end
+
+    registry_manager.register FTML
 
     # TODO: DCASGD
 
